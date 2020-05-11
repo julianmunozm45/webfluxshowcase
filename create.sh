a757a80c3174fc3be678717204281e55b60d6340
Julio45andres <jmunoz@grupoesoft.com>
2020-05-10 23:21:19 -0500

    Implement random email generator

--- create.sh
+++ create.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 port=${2:-8080}
-email=$1
-NAMES=$(curl https://namey.muffinlabs.com/name.json?count=1&with_surname=true&frequency=common)
-nameStr=$(echo $NAMES | cut -d "[" -f2 | cut -d "]" -f1)
-name=$(echo $nameStr | cut -d "\"" -f2 | cut -d "\"" -f1)
-echo $name
-curl -H "content-type: application/json" -d "{\"email\":\"$name\"}" http://localhost:${port}/profiles
+# shellcheck disable=SC2034
+NAMES=$(curl https://namey.muffinlabs.com/name.json?count=1\&with_surname=false\&frequency=common)
+nameStr=$(echo "$NAMES" | cut -d "[" -f2 | cut -d "]" -f1)
+name=$(echo "$nameStr" | cut -d "\"" -f2 | cut -d "\"" -f1)
+email="${name,,}@kmail.com"
+echo "$email"
+curl -H "content-type: application/json" -d "{\"email\":\"$email\"}" http://localhost:"${port}"/profiles
